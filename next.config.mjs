a8283178893cb7eb3dc686c2e4ac3d89af0ba2fb
BrittAbdula <aurorozz@gmail.com>
2025-01-23 12:10:30 +0800

    add 'nodejs_compat'

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 启用 Cloudflare Pages 兼容性
-  experimental: {
-    cloudflarePages: true,
-  },
   // 如果使用了 Images 组件，需要配置
   images: {
     unoptimized: true,
     domains: ['store.genmojionline.com'],
   },
-  output: 'standalone',
+  output: 'export',  // 生成静态网站
+  trailingSlash: true,
 };
 
 export default nextConfig;
